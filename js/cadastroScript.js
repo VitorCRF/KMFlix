83a82ca9a74f4e1a1da7dd24c302d92657c3cd0b
LucasKleaL <lucaskleal222@outlook.com>
2021-03-25 20:34:10 -0300

    Cadastro 1.2
    -Adicionado tela de conclusão de cadastro.
    -Adicionado mais verificações dos campos de cadastro.

--- js/cadastroScript.js
+++ js/cadastroScript.js
@@ -12,14 +12,19 @@ $(document).ready(function(){
         nome = $("#campoNome").val();
         dataNascimento = $("#campoNascimento").val();
         email = $("#campoEmail").val();
-        senha = sjcl.hash.sha256.hash($("#campoSenha").val());
-        confirmarSenha = sjcl.hash.sha256.hash($("#campoConfirmaSenha").val());
 
         verificaNome();
         verificaNascimento();
-        
+        verificaEmail();
         verificaSenha();
         
+        if (prosseguir === true) {
+            enviarInfos();
+            console.log("Prosseguir true")
+        }
+        else {
+            console.log("Prosseguir false")
+        }
 
     });
     
@@ -28,10 +33,12 @@ $(document).ready(function(){
 function verificaNome() {
 
     if (nome != "") {
+        $("#campoNome").removeClass("campo-cadastro-invalido").addClass("campo-cadastro");
         prosseguir = true;
     }
     else {
         prosseguir = false;
+        $("#campoNome").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
         alert("Por favor insira o nome.")
     }
 
@@ -40,10 +47,12 @@ function verificaNome() {
 function verificaNascimento() {
 
     if (dataNascimento != "") {
+        $("#campoNascimento").removeClass("campo-cadastro-invalido").addClass("campo-cadastro");
         prosseguir = true;
     }
     else {
         prosseguir = false;
+        $("#campoNascimento").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
         alert("Por favor insira a data de nascimento.")
     }
 
@@ -52,10 +61,12 @@ function verificaNascimento() {
 function verificaEmail() {
 
     if (email != "") {
+        $("#campoEmail").removeClass("campo-cadastro-invalido").addClass("campo-cadastro");
         prosseguir = true;
     }
     else {
         prosseguir = false;
+        $("#campoEmail").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
         alert("Por favor insira o email.")
     }
 
@@ -63,16 +74,36 @@ function verificaEmail() {
 
 function verificaSenha() {
 
-    if (JSON.stringify(senha) === JSON.stringify(confirmarSenha)) {
+    if ($("#campoSenha").val() != "" && $("#campoConfirmaSenha").val() != "") {
+        senha = sjcl.hash.sha256.hash($("#campoSenha").val());
+        confirmarSenha = sjcl.hash.sha256.hash($("#campoConfirmaSenha").val());
+        confirmaSenha();
+        console.log("Senha não vazia")
+    }
+    else {
+        $("#campoSenha").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
+        $("#campoConfirmaSenha").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
+        console.log("Senha Vazia")
+    }
+
+}
+
+function confirmaSenha() {
+
+    if (JSON.stringify(senha) == JSON.stringify(confirmarSenha)) {
+        $("#campoSenha").removeClass("campo-cadastro-invalido").addClass("campo-cadastro");
+        $("#campoConfirmaSenha").removeClass("campo-cadastro-invalido").addClass("campo-cadastro");
         prosseguir = true;
-        console.log("deu boa");
+        console.log("Verificação de senha completa")
     }
     else {
         prosseguir = false;
+        $("#campoConfirmaSenha").removeClass("campo-cadastro").addClass("campo-cadastro-invalido");
         alert("A senha não corresponde a confirmação.")
     }
 
 }
 
+function enviarInfos() {
 
-
+}
