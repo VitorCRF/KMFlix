282c52b38c179f22705dc62e16dee7d70d8170e9
LucasKleaL <lucaskleal222@outlook.com>
2021-04-05 21:54:34 -0300

    Login 1.2.1
    -Adicionado tela de confirmação para o envio de email para recuperação de senha.
    -Adicionado funcionalidade para solicitar o envio de um novo email para recuperação de senha.

--- js/recuperarSenha.js
+++ js/recuperarSenha.js
@@ -1,20 +1,68 @@
+var email = "";
 
 $(document).ready(function(){
 
     $(".botao-prosseguir").click(function() {
         comunicaPhp();
     })
+
+    $(".botao-novo-email").click(function() {
+        reenviarEmail();
+    })
     
 });
 
 function comunicaPhp() {
-    
-    $.ajax({
-        type: "POST",
-        url: "../php/recuperarSenha.php",
-        data: { emailDestinatario: $("#campoEmail").val()},
 
-        sucees: console.log("dados enviados para o php.")
-    })
+    if (email === "") {
+
+        email = $("#campoEmail").val();
+
+        sessionStorage.setItem("email", email);
+
+        $.ajax({
+            type: "POST",
+            url: "../php/recuperarSenha.php",
+            data: { emailDestinatario: email},
+    
+            sucees: emailSucess()
+        })
+
+    }
+    else {
+
+        $.ajax({
+            type: "POST",
+            url: "../php/recuperarSenha.php",
+            data: { emailDestinatario: email},
+    
+            sucees: emailSucess()
+        })
+
+    }
+
+}
+
+function emailSucess() {
+
+    window.location="../html/confirmacaoRecuperarSenha.html";
+
+}
+
+function reenviarEmail() {
+
+    if (sessionStorage.getItem("email") != "") {
+
+        email = sessionStorage.getItem("email");
+
+        $.ajax({
+            type: "POST",
+            url: "../php/recuperarSenha.php",
+            data: { emailDestinatario: email},
+    
+            sucees: console.log("Email reenviado.")
+        })
+
+    }
 
 }
