3b796218a63b97bcb738ca60a94d0123083fdd07
tiagofelipemuller <tiagofelipemuller@gmail.com>
2021-04-08 20:14:49 -0300

    teste

--- php/Login.php
+++ php/Login.php
@@ -1,14 +1,17 @@
-
 <?php
 
 require_once "config.php";
 
  	$senha = $_POST["campoLoginEmail"];
 	$email = $_POST["campoLoginSenha"];
-
-	$sql = mysqli_query($link,"SELECT senha FROM user where email = $email");
-
-	echo "$sql";
-			//header("location: ../index.html");
-			//exit();
+	$sql = "SELECT * FROM user";
+         
+	if($stmt = mysqli_prepare($link, $sql)){
+		
+		if(mysqli_stmt_execute($stmt)){
+			echo "$sql";
+		} else{
+			echo "OUps! Algo deu errado. Por favor, tente novamente mais tarde.";
+		}
+	} 
 ?>
