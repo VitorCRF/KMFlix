76198350127527e43b49476867817bd1f161235e
LucasKleaL <lucaskleal222@outlook.com>
2021-04-24 18:57:13 -0300

    Home 1.2.1
    -Adicionado barra com informações em cada titulo ao sobrepor o mouse no card.

--- js/home.js
+++ js/home.js
@@ -2,10 +2,43 @@
 var arrayTitulos = [['Breaking Bad', ]]
 var searchInputIsActive = false;
 var textInput = "";
+var infoSlideIsActive = false;
+var hoverIsActive = false;
 
 $(document).ready(function() {
 
     carouselSlick();
+
+    var content = "";
+
+    $("#imagemCarousel1").hover(function() {
+        
+        if (infoSlideIsActive === false) {
+
+            content += '<img src="../public/icons/white_play_button.png" class="play-button" alt="play button" id="playButton">';
+            content += '<h1 class="h1-titulo" id="titulo">Breaking Bad</h1>';
+            content += '<p class="p-ano-lancamento" id="ano">2013</p>';
+            content += '<p class="p-genero1" id="genero1">Suspense</p>';
+            content += '<p class="p-genero1" id="genero2">Drama</p>'
+
+            $(".div-infos-slide").append(content);
+            $(".div-infos-slide").removeClass("div-infos-slide").addClass("div-infos-slide-ativa");
+            content = "";
+            infoSlideIsActive = true;
+            console.log(infoSlideIsActive)
+        }
+        else {
+            $("#playButton").remove();
+            $("#titulo").remove();
+            $("#ano").remove();
+            $("#genero1").remove();
+            $("#genero2").remove();
+            $(".div-infos-slide-ativa").removeClass("div-infos-slide-ativa").addClass("div-infos-slide");
+            infoSlideIsActive = false;
+            console.log(infoSlideIsActive)
+        }
+
+    })
 
 })
 
@@ -19,7 +52,6 @@ function carouselSlick() {
         variableWidth: true,
         adaptiveHeight: true,
         
-
         responsive: [
 
             {
